d6a323348527d41210d3cf89ba7b4a39f3d536cc
dytna <dytnal@gmail.com>
2025-01-04 12:58:13 -0600

    first commit

---
+++ api-server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const { auth } = require("express-oauth2-jwt-bearer");
+const authConfig = require("./src/auth_config.json");
+
+const app = express();
+
+const port = process.env.API_PORT || 3001;
+const appPort = process.env.SERVER_PORT || 3000;
+const appOrigin = authConfig.appOrigin || `http://localhost:${appPort}`;
+
+if (
+  !authConfig.domain ||
+  !authConfig.audience ||
+  authConfig.audience === "YOUR_API_IDENTIFIER"
+) {
+  console.log(
+    "Exiting: Please make sure that auth_config.json is in place and populated with valid domain and audience values"
+  );
+
+  process.exit();
+}
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors({ origin: appOrigin, origin: 8100 }));
+
+
+const checkJwt = auth({
+  audience: "https://api-dev.spotmine.io",
+  issuerBaseURL: `https://spotmine-dev.auth0.com/`,
+  algorithms: ["RS256"],
+});
+
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your access token was successfully validated!",
+  });
+});
+
+app.listen(port, () => console.log(`API Server listening on port ${port}`));
